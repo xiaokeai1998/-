54208c903d567e061ab4f98cbff495aedfc856b4
unknown <wumenx>
2019-07-09 13:51:25 +0800

    后台电商角色授权的完成

--- src/views/right/RoleList.vue
+++ src/views/right/RoleList.vue
@@ -17,22 +17,29 @@
             v-for="first in scope.row.children"
             :key="first.id"
             style="margin-bottom:10px;border-bottom:1px #ccc dashed"
-             @close='delRight(scope.row,first.id)'
+            @close="delRight(scope.row,first.id)"
           >
-            <el-col :span="4">
+            <el- col :span="4">
               <el-tag closable type="warning">{{first.authName}}</el-tag>
-            </el-col>
+            </el->
             <el-col :span="20">
-              <el-row v-for="second in first.children" :key="second.id" style="margin-bottom:6px"  @close='delRight(scope.row,second.id)'>
+              <el-row
+                v-for="second in first.children"
+                :key="second.id"
+                style="margin-bottom:6px"
+                @close="delRight(scope.row,second.id)"
+              >
                 <el-col :span="4">
                   <el-tag closable type="success">{{second.authName}}</el-tag>
                 </el-col>
-                <el-col :span="20" >
+                <el-col :span="20">
                   <el-tag
                     closable
                     type="info"
                     v-for="third in second.children"
-                    :key="third.id" style="margin-right:4px;margin-bottom:6px " @close='delRight(scope.row,third.id)'
+                    :key="third.id"
+                    style="margin-right:4px;margin-bottom:6px "
+                    @close="delRight(scope.row,third.id)"
                   >{{third.authName}}</el-tag>
                 </el-col>
               </el-row>
@@ -40,7 +47,7 @@
           </el-row>
 
           <el-row>
-            <el-col :span='24' v-show="scope.row.children.length===0">没有任何数据，请去设置权限</el-col>
+            <el-col :span="24" v-show="scope.row.children.length===0">没有任何数据，请去设置权限</el-col>
           </el-row>
         </template>
       </el-table-column>
@@ -56,7 +63,7 @@
           </el-tooltip>
 
           <el-tooltip content="分配角色" placement="top">
-            <el-button type="primary" icon="el-icon-check"></el-button>
+            <el-button type="primary" icon="el-icon-check" @click='showdialogForm'></el-button>
           </el-tooltip>
 
           <el-tooltip content="删除" placement="top">
@@ -66,16 +73,41 @@
       </el-table-column>
     </el-table>
 
-    <!-- 数据表格的展示 -->
+    <!-- 添加分配权限对话框 -->
+    <el-dialog title="授权角色" :visible.sync="grantdialogFormVisible">
+      <el-tree
+      :data="rightList"
+      show-checkbox
+      node-key="id"
+      :default-expand-all ='true'
+      :default-checked-keys="checkedArr"
+      :props="defaultProps">
+    </el-tree>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="grantdialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="grantdialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
 import { getAllRoleLlist, delRoleById } from '@/api/role_api.js'
+import { getAllRightList } from '@/api/right_api.js'
 export default {
   data () {
     return {
+      grantdialogFormVisible: false,
       roleId: '',
-      roleList: []
+      roleList: [],
+      addrole: [],
+      // 应该默认被选中的节点id数组
+      checkedArr: [],
+      rightList: [],
+      defaultProps: {
+        children: 'children',
+        label: 'authName'
+      }
+
     }
   },
 
@@ -85,6 +117,14 @@ export default {
   },
 
   methods: {
+    // 展示角色授权对话框
+    showdialogForm () {
+      this.grantdialogFormVisible = true
+      getAllRightList('tree')
+        .then(res => {
+          this.rightList = res.data.data
+        })
+    },
     // 删除指定角色的权限
     // 如果是删除三级权限，那么仅仅删除三级权限，
     delRight (row, rightId) {
@@ -95,7 +135,7 @@ export default {
         if (res.data.meta.status === 200) {
           this.$message({
             type: 'success',
-            message: res.data.data
+            message: '删除成功'
           })
           // 重置当前展开的数据源数组
           row.children = res.data.data
@@ -106,7 +146,7 @@ export default {
       getAllRoleLlist()
         .then(res => {
           if (res.data.meta.status === 200) {
-          // console.log(res)
+            // console.log(res)
             this.roleList = res.data.data
           }
         })
@@ -115,7 +155,6 @@ export default {
         })
     }
   }
-
 }
 </script>
 <style lang="less" scoped>
